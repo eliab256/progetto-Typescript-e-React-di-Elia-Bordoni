fc8bc9e53fbe19d8a57dc6533112525df1c6023c
b-elia <bordonielia96@gmail.com>
2024-09-23 15:16:58 +0200

    creazione pagina quizpages con stili e dimensioni

--- src/Components/QuizPages.tsx
+++ src/Components/QuizPages.tsx
@@ -3,8 +3,13 @@ import "../assets/Styles/quizPages.css"
 const QuizPages: React.FC = () => {
 
     return(
-        <div className="cardContainer">
-            <h1>hello</h1>
+        <div className="cardContainer widthController">
+            <div className="loadingBar"></div>
+            <div className="cardSection"></div>
+            <div className="questionsButton">
+                <button /*onClick={}*/>Comeback</button>
+                <button /*onClick={}*/>Next question</button>  
+            </div>
         </div>
     );
 };
