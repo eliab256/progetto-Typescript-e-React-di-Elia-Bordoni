e8ca8bd9557066157f477cd53eb3755e883fa1fb
b-elia <bordonielia96@gmail.com>
2024-10-04 16:56:04 +0200

    aggiustamento link main.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './Store';
 import App from './App.tsx';
-import './assets/Styles/Main.css';
+import './assets/Styles/main.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
